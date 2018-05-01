585ede29a531409e6934b32f2f7c68cf8490951c
Burrypony <kiril.chernysh@gmail.com>
2018-05-01 11:43:18 +0300

    timer

--- www/js/common.js
+++ www/js/common.js
@@ -364,4 +364,34 @@ $('.set-button').click(function(e){
     })
 
     // ===================== SOCIAL ======================
+    // Set the date we're counting down to
+var countDownDate = new Date("May 2, 2018 11:50:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance <= 1000000)
+  {
+    countDownDate += 10000000;
+  }
+
+}, 1000);
+
 });
