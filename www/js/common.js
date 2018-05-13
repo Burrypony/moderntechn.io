0c354cbdf2ec6ee61e481d8d6c304a5a0eac8874
Burrypony <kiril.chernysh@gmail.com>
2018-05-13 22:47:07 +0300

    timmer settings

--- www/js/common.js
+++ www/js/common.js
@@ -373,7 +373,7 @@ $('.set-button').click(function(e){
 
     // ===================== SOCIAL ======================
     // Set the date we're counting down to
-var countDownDate = new Date("May 6, 2018 11:50:00").getTime();
+var countDownDate = new Date("May 15, 2018 11:50:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
